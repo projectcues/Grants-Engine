b9803fd5a726549f40cf1828c345a403018c0003
Lord Almighty <lordalmighty@Lords-Mac-mini.lan>
2026-05-25 12:12:49 -0400

    fix: nest serverActions in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   serverExternalPackages: [],
-  serverActions: {
-    allowedOrigins: ["*.projectcues.com", "localhost:3000"]
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*.projectcues.com", "localhost:3000"]
+    }
   }
 };
 
